ceb88a3d0d4b6747174b20a7cd6ef00436b5fdbb
huhao <415328529@qq.com>
2018-07-25 17:02:50 +0800

    优化FileObjectCopier，添加AliyunOSSVirtualFileSystemBuilder

--- src/main/java/cc/whohow/aliyun/oss/FileObjects.java
+++ src/main/java/cc/whohow/aliyun/oss/FileObjects.java
@@ -27,12 +27,32 @@ public class FileObjects {
     /**
      * 获取文件对象
      */
-    public static FileObject resolve(String uri) {
+    public static FileObject get(String uri) {
         try {
             return VFS.getManager().resolveFile(uri);
         } catch (FileSystemException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    /**
+     * 获取文件公开链接
+     */
+    public static String getPublicURIString(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return fileObject.getPublicURIString();
+        } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 获取输出流
+     */
+    public static InputStream newInputStream(String uri) throws IOException {
+        FileObject fileObject = get(uri);
+        FileContent fileContent = fileObject.getContent();
+        return new ResourceInputStream(fileContent, new ResourceInputStream(fileContent, fileContent.getInputStream()));
     }
 
     /**
@@ -46,9 +66,29 @@ public class FileObjects {
     /**
      * 获取输入流
      */
+    public static OutputStream newOutputStream(String uri) throws IOException {
+        FileObject fileObject = get(uri);
+        FileContent fileContent = fileObject.getContent();
+        return new ResourceOutputStream(fileObject, new ResourceOutputStream(fileContent, fileContent.getOutputStream()));
+    }
+
+    /**
+     * 获取输入流
+     */
     public static OutputStream newOutputStream(FileObject fileObject) throws IOException {
         FileContent fileContent = fileObject.getContent();
         return new ResourceOutputStream(fileContent, fileContent.getOutputStream());
+    }
+
+    /**
+     * 获取文件后缀，若无，返回空字符串
+     */
+    public static String getSuffix(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return getSuffix(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     /**
@@ -72,8 +112,30 @@ public class FileObjects {
     /**
      * 创建临时文件
      */
+    public static FileObject createTempFile(String dir, String suffix) {
+        try (FileObject fileObject = get(dir)) {
+            return createTempFile(fileObject, suffix);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 创建临时文件
+     */
     public static FileObject createTempFile(FileObject dir, String suffix) {
         return createTempFile(dir, null, suffix);
+    }
+
+    /**
+     * 创建临时文件
+     */
+    public static FileObject createTempFile(String dir, String prefix, String suffix) {
+        try (FileObject fileObject = get(dir)) {
+            return createTempFile(fileObject, prefix, suffix);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     /**
@@ -105,6 +167,17 @@ public class FileObjects {
     /**
      * 删除文件
      */
+    public static void delete(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            delete(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 删除文件
+     */
     public static void delete(FileObject fileObject) {
         try {
             fileObject.deleteAll();
@@ -116,10 +189,32 @@ public class FileObjects {
     /**
      * 删除文件，忽略所有错误
      */
+    public static void deleteQuietly(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            deleteQuietly(fileObject);
+        } catch (IOException ignore) {
+        }
+    }
+
+    /**
+     * 删除文件，忽略所有错误
+     */
     public static void deleteQuietly(FileObject fileObject) {
         try {
             fileObject.deleteAll();
         } catch (Exception ignore) {
+        }
+    }
+
+    /**
+     * 拷贝文件
+     */
+    public static void copy(String source, String target) {
+        try (FileObject sourceFileObject = get(source);
+             FileObject targetFileObject = get(target)) {
+            copy(sourceFileObject, targetFileObject);
+        } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
         }
     }
 
@@ -137,10 +232,33 @@ public class FileObjects {
     /**
      * 移动文件
      */
+    public static void move(String source, String target) {
+        try (FileObject sourceFileObject = get(source);
+             FileObject targetFileObject = get(target)) {
+            copy(sourceFileObject, targetFileObject);
+        } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 移动文件
+     */
     public static void move(FileObject source, FileObject target) {
         try {
             source.moveTo(target);
         } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 猜测文件类型
+     */
+    public static String guessContentType(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return guessContentType(fileObject);
+        } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
@@ -162,10 +280,32 @@ public class FileObjects {
     /**
      * 读取文件属性
      */
+    public static Map<String, Object> readAttributes(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return readAttributes(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 读取文件属性
+     */
     public static Map<String, Object> readAttributes(FileObject fileObject) {
         try (FileContent fileContent = fileObject.getContent()) {
             return fileContent.getAttributes();
         } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 是否是目录
+     */
+    public static boolean isDirectory(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return isDirectory(fileObject);
+        } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
@@ -184,6 +324,17 @@ public class FileObjects {
     /**
      * 是否是文件
      */
+    public static boolean isRegularFile(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return isRegularFile(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 是否是文件
+     */
     public static boolean isRegularFile(FileObject fileObject) {
         try {
             return fileObject.isFile();
@@ -195,10 +346,32 @@ public class FileObjects {
     /**
      * 获取文件最后修改时间
      */
+    public static long getLastModifiedTime(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return getLastModifiedTime(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 获取文件最后修改时间
+     */
     public static long getLastModifiedTime(FileObject fileObject) {
         try (FileContent fileContent = fileObject.getContent()) {
             return fileContent.getLastModifiedTime();
         } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 获取文件大小
+     */
+    public static long size(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return size(fileObject);
+        } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
@@ -228,10 +401,32 @@ public class FileObjects {
     /**
      * 文件是否存在
      */
+    public static boolean exists(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return exists(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 文件是否存在
+     */
     public static boolean exists(FileObject fileObject) {
         try {
             return fileObject.exists();
         } catch (FileSystemException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 文件是否不存在
+     */
+    public static boolean notExists(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return notExists(fileObject);
+        } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
     }
@@ -246,18 +441,53 @@ public class FileObjects {
     /**
      * 拷贝流到文件
      */
-    public static void copy(InputStream source, FileObject target) throws IOException {
+    public static void copy(InputStream source, String target) {
+        try (FileObject fileObject = get(target)) {
+            copy(source, fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 拷贝流到文件
+     */
+    public static void copy(InputStream source, FileObject target) {
         copy(new StreamFileObjectAdapter(source), target);
     }
 
     /**
      * 拷贝文件到流
      */
-    public static void copy(FileObject source, OutputStream target) throws IOException {
+    public static void copy(String source, OutputStream target) {
+        try (FileObject fileObject = get(source)) {
+            copy(fileObject, target);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 拷贝文件到流
+     */
+    public static void copy(FileObject source, OutputStream target) {
         try (FileContent fileContent = source.getContent()) {
             try (Java9InputStream stream = new Java9InputStream(fileContent.getInputStream())) {
                 stream.transferTo(target);
             }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 读取所有字节
+     */
+    public static byte[] readAllBytes(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return readAllBytes(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 
@@ -269,6 +499,17 @@ public class FileObjects {
             try (Java9InputStream stream = new Java9InputStream(fileContent.getInputStream())) {
                 return stream.readAllBytes();
             }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 读取所有字节到缓冲区
+     */
+    public static ByteBuffer read(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return read(fileObject);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
@@ -290,9 +531,27 @@ public class FileObjects {
     /**
      * 写入文件
      */
+    public static void write(String uri, byte[] bytes) {
+        try (FileObject fileObject = get(uri)) {
+            write(fileObject, bytes);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 写入文件
+     */
     public static void write(FileObject fileObject, byte[] bytes) {
-        try {
-            copy(new ByteArrayInputStream(bytes), fileObject);
+        copy(new ByteArrayInputStream(bytes), fileObject);
+    }
+
+    /**
+     * 写入文件
+     */
+    public static void write(String uri, ByteBuffer bytes) {
+        try (FileObject fileObject = get(uri)) {
+            write(fileObject, bytes);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
@@ -302,8 +561,15 @@ public class FileObjects {
      * 写入文件
      */
     public static void write(FileObject fileObject, ByteBuffer bytes) {
-        try {
-            copy(new ByteBufferInputStream(bytes), fileObject);
+        copy(new ByteBufferInputStream(bytes), fileObject);
+    }
+
+    /**
+     * 读取字符串
+     */
+    public static String read(String uri, Charset charset) {
+        try (FileObject fileObject = get(uri)) {
+            return read(fileObject, charset);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
@@ -319,8 +585,30 @@ public class FileObjects {
     /**
      * 写入字符串
      */
+    public static void write(String uri, CharSequence text, Charset charset) {
+        try (FileObject fileObject = get(uri)) {
+            write(fileObject, text, charset);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 写入字符串
+     */
     public static void write(FileObject fileObject, CharSequence text, Charset charset) {
         write(fileObject, charset.encode(CharBuffer.wrap(text)));
+    }
+
+    /**
+     * 读取utf-8字符串
+     */
+    public static String readUtf8(String uri) {
+        try (FileObject fileObject = get(uri)) {
+            return readUtf8(fileObject);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     /**
@@ -333,8 +621,27 @@ public class FileObjects {
     /**
      * 写入utf-8字符串
      */
+    public static void writeUtf8(String uri, CharSequence text) {
+        try (FileObject fileObject = get(uri)) {
+            writeUtf8(fileObject, text);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * 写入utf-8字符串
+     */
     public static void writeUtf8(FileObject fileObject, CharSequence text) {
         write(fileObject, text, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 列出文件夹下所有文件
+     */
+    public static Stream<FileObject> list(String uri) throws IOException {
+        FileObject fileObject = get(uri);
+        return list(fileObject).onClose(new UncheckedRunnable(fileObject));
     }
 
     /**
@@ -347,6 +654,14 @@ public class FileObjects {
     /**
      * 递归列出文件夹下所有文件
      */
+    public static Stream<FileObject> walk(String uri) throws IOException {
+        FileObject fileObject = get(uri);
+        return walk(fileObject).onClose(new UncheckedRunnable(fileObject));
+    }
+
+    /**
+     * 递归列出文件夹下所有文件
+     */
     public static Stream<FileObject> walk(FileObject fileObject) throws IOException {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(fileObject.iterator(), 0), false);
     }
@@ -354,21 +669,22 @@ public class FileObjects {
     /**
      * 按行读取文件
      */
-    public static Stream<String> lines(FileObject fileObject, Charset charset) {
+    public static Stream<String> lines(String uri, Charset charset) throws IOException {
+        FileObject fileObject = get(uri);
+        return lines(fileObject, charset).onClose(new UncheckedRunnable(fileObject));
+    }
+
+    /**
+     * 按行读取文件
+     */
+    public static Stream<String> lines(FileObject fileObject, Charset charset) throws IOException {
         BufferedReader reader = null;
         try {
-            try {
-                reader = new BufferedReader(new InputStreamReader(newInputStream(fileObject), charset));
-                return reader.lines().onClose(new UncheckedRunnable(reader));
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
+            reader = new BufferedReader(new InputStreamReader(newInputStream(fileObject), charset));
+            return reader.lines().onClose(new UncheckedRunnable(reader));
         } catch (RuntimeException e) {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException ignore) {
+            if (reader != null) {
+                reader.close();
             }
             throw e;
         }
--- src/main/java/cc/whohow/aliyun/oss/vfs/copy/FileObjectCopier.java
+++ src/main/java/cc/whohow/aliyun/oss/vfs/copy/FileObjectCopier.java
@@ -4,8 +4,6 @@ import org.apache.commons.vfs2.*;
 import org.apache.commons.vfs2.operations.FileOperation;
 
 import java.io.UncheckedIOException;
-import java.util.IdentityHashMap;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.function.BiFunction;
@@ -15,12 +13,11 @@ import java.util.function.Function;
  * 文件拷贝工具
  */
 public class FileObjectCopier implements Callable<FileObject> {
-    private FileObject source;
-    private FileObject target;
-    private FileSelector selector = Selectors.SELECT_ALL;
-    private IdentityHashMap<FileObject, Boolean> refs = new IdentityHashMap<>();
+    protected FileObject source;
+    protected FileObject target;
+    protected FileSelector selector = Selectors.SELECT_ALL;
 
-    public FileObject resolve(String uri) {
+    protected FileObject resolve(String uri) {
         try {
             return VFS.getManager().resolveFile(uri);
         } catch (FileSystemException e) {
@@ -30,31 +27,26 @@ public class FileObjectCopier implements Callable<FileObject> {
 
     public FileObjectCopier setSource(String source) {
         this.source = resolve(source);
-        refs.putIfAbsent(this.source, Boolean.TRUE);
         return this;
     }
 
     public FileObjectCopier setSource(FileObject source) {
         this.source = source;
-        refs.putIfAbsent(this.source, Boolean.FALSE);
         return this;
     }
 
     public FileObjectCopier setTarget(String target) {
         this.target = resolve(target);
-        refs.putIfAbsent(this.target, Boolean.FALSE);
         return this;
     }
 
     public FileObjectCopier setTarget(FileObject target) {
         this.target = target;
-        refs.putIfAbsent(this.target, Boolean.FALSE);
         return this;
     }
 
     public FileObjectCopier withSource(Function<FileObject, FileObject> function) {
         this.source = function.apply(source);
-        refs.putIfAbsent(this.source, Boolean.TRUE);
         return this;
     }
 
@@ -63,7 +55,6 @@ public class FileObjectCopier implements Callable<FileObject> {
                                                                            BiFunction<FileObject, OP, FileObject> function) {
         try {
             this.source = function.apply(source, (OP) source.getFileOperations().getOperation(operation));
-            refs.putIfAbsent(this.source, Boolean.TRUE);
             return this;
         } catch (FileSystemException e) {
             throw new UncheckedIOException(e);
@@ -72,7 +63,6 @@ public class FileObjectCopier implements Callable<FileObject> {
 
     public FileObjectCopier withTarget(BiFunction<FileObject, FileObject, FileObject> function) {
         this.target = function.apply(source, target);
-        refs.putIfAbsent(this.target, Boolean.TRUE);
         return this;
     }
 
@@ -93,15 +83,6 @@ public class FileObjectCopier implements Callable<FileObject> {
     }
 
     protected void close() {
-        for (Map.Entry<FileObject, Boolean> e : refs.entrySet()) {
-            try {
-                if (e.getValue()) {
-                    e.getKey().close();
-                }
-            } catch (Exception ignore) {
-            }
-        }
-        refs.clear();
     }
 
     protected String newRandomName(FileObject fileObject) {
